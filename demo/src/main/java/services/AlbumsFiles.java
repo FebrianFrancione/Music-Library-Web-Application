72cb28c73f5678927ffa876713ec79bc06959ba8
LettuceFoot <febrian.francione@gmail.com>
2021-03-05 10:36:55 -0500

    Added delete option

--- demo/src/main/java/services/AlbumsFiles.java
+++ demo/src/main/java/services/AlbumsFiles.java
@@ -9,6 +9,7 @@ import java.io.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,7 +18,8 @@ import java.io.InputStream;
 @Path("/files")
 public class AlbumsFiles {
 
-    private static final String FILE_PATH = "C:\\Users\\febri\\IdeaProjects\\SOEN-487-A2\\demo\\pom.xml";
+    private static final String FILE_PATH = "demo/src/main/java/savedFiles/test.txt";
+//    private static final String FILE_PATH = "C:\\Users\\febri\\IdeaProjects\\SOEN-487-A2\\demo\\pom.xml";
 
     /**
      * The API method will return the file located at FILE_PATH. Modify the path to return whatever
@@ -38,7 +40,7 @@ public class AlbumsFiles {
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public void uploadFile(@FormDataParam("file") InputStream uploadedInputStream) {
-        String fileLocation = "demo/src/main/java/savedFiles/test.txt"; // file to save to
+        String fileLocation = "demo/src/main/java/savedFiles/"; // file to save to
         //saving file
         try {
             int read = 0;
@@ -51,5 +53,23 @@ public class AlbumsFiles {
             out.close();
         } catch (IOException e) {e.printStackTrace();}
     }
+
+    @DELETE
+    @Produces({MediaType.TEXT_PLAIN})
+    @Path("/delete")
+    public void deleteAlbum() {
+        File file = new File(FILE_PATH);
+        file.delete();
+//        if(albumsManager.deleteAlbum(ISRC)){
+//            message = "Album " + ISRC + " successfully deleted!";
+//            return Response.ok(message).build();
+//        }
+//
+//        else{
+//            message = "Album " + ISRC + " was not found";
+//            return Response.status(Response.Status.NOT_FOUND).entity(message).build();
+//        }
+    }
+
 
 }
