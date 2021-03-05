04f8c9f6ab980508592b68c87799dd664da9a9d3
ivan <garzon861@gmail.com>
2021-03-05 18:05:14 -0500

    adding mysql dependency

--- demo/src/main/java/persistence/JDBCTest.java
+++ demo/src/main/java/persistence/JDBCTest.java
@@ -8,7 +8,6 @@ public class JDBCTest {
     public static void main(String[] args) throws Exception {
         AlbumDao albumDao = new AlbumDao();
         StringBuilder albumsString= new StringBuilder();
-        ArrayList<Album> albums = albumDao.getAlbums();
 
         System.out.println("\nInserting album with ISRC new_alb");
         albumDao.insertAlbum("new_alb","random_title","new desc",2000,"Sam","Doe", "test_image","image/jpeg");
@@ -20,18 +19,20 @@ public class JDBCTest {
         System.out.println("\nUpdating album with ISRC new_alb");
         albumDao.updateAlbum("new_alb","89title","updated desc",2020,"Ivan","Garzon");
 
+        System.out.println("\nGetting all albums");
+        ArrayList<Album> albums = albumDao.getAlbums();
+
         if(!albums.isEmpty()){
             for(Album a: albums)
                 albumsString.append(a.toString()).append("\n");
             System.out.println(albumsString);
         }
 
-        /*
+
         System.out.println("\nDeleting album with ISRC new_alb");
-        if(albumDao.deleteAlbum("new_alb"))
+        if(albumDao.deleteAlbum("89album"))
             System.out.println("Album deleted");
 
-         */
 
     }
 
